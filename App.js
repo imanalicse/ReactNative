abf0134719d255f7e32140cbb5de758693d2eaf0
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-04-10 00:40:56 +0600

    networking

--- App.js
+++ App.js
@@ -5,20 +5,64 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet,Text, View 
-} from 'react-native';
+import { Platform, StyleSheet,Text, View, FlatList, ActivityIndicator } from 'react-native';
 
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true      
+    }
+  }
+
+  componentDidMount(){
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>        
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return(
+      <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={(item, index) => index}
+        />
       </View>
     );
+
+    // return (
+    //   <View style={styles.container}>
+    //     <Text style={styles.welcome}>
+    //       Welcome to React Native!
+    //     </Text>        
+    //   </View>
+    // );
   }
 }
 
